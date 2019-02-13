ebf4a1c1ebd098d45ab7c93ad7a4d35677d149d1
Adam <adamandrewpate@gmail.com>
2019-02-12 23:03:41 -0600

    Added members to abstract card class

--- app/src/main/java/com/apps/adam/decelerateanxietymanager2/cards/Card.java
+++ app/src/main/java/com/apps/adam/decelerateanxietymanager2/cards/Card.java
@@ -2,5 +2,15 @@ package com.apps.adam.decelerateanxietymanager2.cards;
 
 public abstract class Card
 {
+    private String cardName;
 
+    public String getCardName()
+    {
+        return cardName;
+    }
+
+    public void setCardName(String cardName)
+    {
+        this.cardName = cardName;
+    }
 }
