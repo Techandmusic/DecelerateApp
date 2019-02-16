808951908e2ae7cfa7d3b4ceca3e80ded2a43be4
Adam <adamandrewpate@gmail.com>
2019-02-16 01:00:05 -0600

    Created GratitudeFragment class.

---
+++ app/src/main/java/com/apps/adam/decelerateanxietymanager2/fragments/GratitudeFragment.java
@@ -0,0 +1,12 @@
+package com.apps.adam.decelerateanxietymanager2.fragments;
+
+public class GratitudeFragment extends BaseFragment
+{
+    public static final String BOOK_ID = "Gratitude";
+
+    //Class constructor
+    public GratitudeFragment()
+    {
+        setBookName(BOOK_ID);
+    }
+}
